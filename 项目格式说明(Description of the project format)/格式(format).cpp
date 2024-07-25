4fd9880fcac9a3efb6a1e664667ab77c0c798fbf
msy1332 <17322714918@163.com>
2024-07-25 09:25:48 +0800

    第一次提交框架(Submit the framework for the first time)

---
+++ 项目格式说明(Description of the project format)/格式(format).cpp
@@ -0,0 +1,21 @@
+/*
+    题目：
+    作者：
+    时间：
+*/
+#include <iostream>//头文件(header files)
+#include <stdlib.h>//防闪退头文件(Anti-flashback header files)
+using namespace std;
+void Method1()//方法一
+{
+
+}
+void Method2()//方法二
+{
+
+}
+int main()//主函数(Main function)
+{
+    system("pause");//防闪退(Anti-flashback)
+    return 0;
+}
